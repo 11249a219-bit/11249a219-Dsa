c99099991e27b6ab3025e925d6f0e28532e8b024
11249a219-bit <11249a219@kanchiuniv.ac.in>
2026-04-25 22:30:07 +0530

    Update Kruskal's algorithm.c

--- DAA/Kruskal's algorithm.c
+++ DAA/Kruskal's algorithm.c
@@ -2,9 +2,7 @@
 #define V 5 
 #define E 6 
 int parent[V]; 
- 
-int findLeader(int v) 
-{ 
+int findLeader(int v) {
     while (parent[v] != v) 
         v = parent[v]; 
     return v; 
@@ -33,22 +31,18 @@ void sortEdges(int edges[E][3])
         } 
     } 
 } 
- 
 void kruskal(int edges[E][3]) 
 { 
     sortEdges(edges); 
     for (int i = 0; i < V; i++) 
         parent[i] = i; 
- 
     int count = 0, cost = 0; 
     printf("Selected Edges:\n"); 
- 
     for (int i = 0; i < E; i++) 
     { 
         int u = edges[i][0]; 
         int v = edges[i][1]; 
         int w = edges[i][2]; 
-
         if (findLeader(u) != findLeader(v)) 
         { 
             printf("%d -- %d (Weight: %d)\n", u, v, w); 
@@ -61,12 +55,9 @@ void kruskal(int edges[E][3])
     } 
     printf("Total Cost: %d\n", cost); 
 } 
- 
-int main() 
-{ 
+int main() {
     int edges[E][3] = { 
         {0, 1, 2}, {0, 2, 3}, {1, 2, 5}, {1, 3, 3}, {2, 4, 4}, {3, 4, 2}}; 
- 
     kruskal(edges); 
     return 0; 
 }
